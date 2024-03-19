ab925c15ddf0902d560eea3b06cbc7a6627431f7
Brunopuod <80692024+Brundex@users.noreply.github.com>
2024-03-18 23:08:29 -0300

    Update cartRouter.js

--- src/routes/cartRouter.js
+++ src/routes/cartRouter.js
@@ -1,27 +1,74 @@
-import { Router } from "express";
-import { CartManager } from "../config/CartManager.js";
-const cartManager = new CartManager('./src/data/cart.json')
+import { promises as fs } from 'fs'
 
-const cartRouter = Router()
-
-cartRouter.get('/', async (req, res) => {
-    try {
-        const cart = await cartManager.getCart()
-        res.status(200).send(cart)
-    } catch (error) {
-        res.status(500).send(`Error interno del servidor al consultar carrito: ${error}`)
+export class CartManager {
+    constructor(path) {
+        this.products = path
     }
-})
 
-cartRouter.post('/:pid', async (req, res) => {
-    try {
-        const productId = req.params.pid
-        const { quantity } = req.body
-        const mensaje = await cartManager.addProductByCart(productId, quantity)
-        res.status(200).send(mensaje)
-    } catch (error) {
-        res.status(500).send(`Error interno del servidor al crear producto: ${error}`)
+    async getCart() {
+        const cart = JSON.parse(await fs.readFile(this.products, 'utf-8'))
+        return cart
     }
-})
 
-export default cartRouter
+    async addProductByCart(idProducto, quantityParam) {
+        const cart = JSON.parse(await fs.readFile(this.products, 'utf-8'))
+
+        const indice = cart.findIndex(product => product.id == idProducto)
+
+        if (indice != -1) {
+            cart[indice].quantity += quantityParam
+        } else {
+            const prod = { id: idProducto, quantity: quantityParam }
+            cart.push(prod)
+        }
+        await fs.writeFile(this.products, JSON.stringify(cart))
+        return "Producto cargado correctamente"
+    }
+
+    async removeProductFromCart(cartId, productId) {
+        try {
+            const cart = await Cart.findById(cartId);
+            cart.products = cart.products.filter(product => product.id_prod.toString() !== productId);
+            await cart.save();
+            return "Producto eliminado del carrito exitosamente";
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateCart(cartId, products) {
+        try {
+            const cart = await Cart.findByIdAndUpdate(cartId, { products }, { new: true });
+            return "Carrito actualizado exitosamente";
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateProductQuantity(cartId, productId, quantity) {
+        try {
+            const cart = await Cart.findById(cartId);
+            const product = cart.products.find(product => product.id_prod.toString() === productId);
+            if (product) {
+                product.quantity = quantity;
+                await cart.save();
+                return "Cantidad del producto actualizada exitosamente";
+            } else {
+                throw new Error("Producto no encontrado en el carrito");
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async clearCart(cartId) {
+        try {
+            const cart = await Cart.findByIdAndUpdate(cartId, { products: [] }, { new: true });
+            return "Carrito limpiado exitosamente";
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+
