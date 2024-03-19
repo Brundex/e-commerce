eb40212517e8d148350583396fd4824a8daf109b
Brunopuod <80692024+Brundex@users.noreply.github.com>
2024-03-18 23:07:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
 import express from 'express'
-import cartRouter from './routes/cartRouter.js'
-import productsRouter from './routes/productsRouter.js'
+import mongoose from 'mongoose'
 import { Server } from 'socket.io'
 import { engine } from 'express-handlebars'
 import { __dirname } from './path.js'
 
+//Configuraciones o declaraciones
 const app = express()
-const PORT = 8080
+const PORT = 8000
 
+//Server
 const server = app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
 })
 
 const io = new Server(server)
 
+//Connection DB
+mongoose.connect("mongodb+srv://puodziunasb:9xV!wL9VnHeEqg@cluster0.s05wzm8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log("DB is connected")).catch(e => console.log(e))
+
+//Middlewares
 app.use(express.json())
+app.use(session({
+    secret: "coderSecret",
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(cookieParser("claveSecreta"))
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', __dirname + '/views')
 
+app.use('/', indexRouter)
 
-//conexion a socket.io vista en clase
+
 io.on('connection', (socket) => {
     console.log("Conexion con Socket.io")
 
-    socket.on('movimiento', info => { 
-        console.log(info)
-    })
+    socket.on('mensaje', async (mensaje) => {
+        try {
+            await messageModel.create(mensaje)
+            const mensajes = await messageModel.find()
+            io.emit('mensajeLogs', mensajes)
+        } catch (e) {
+            io.emit('mensajeLogs', e)
+        }
 
-    socket.on('rendirse', info => { 
-        console.log(info)
-        socket.emit('mensaje-jugador', "Te has rendido") 
-        socket.broadcast.emit('rendicion', "El jugador se rindio") 
     })
 
 })
-
-app.use('/static', express.static(__dirname + '/public'))
-app.use('/api/products', productsRouter, express.static(__dirname + '/public'))
-app.use('/api/cart', cartRouter)
-
-
